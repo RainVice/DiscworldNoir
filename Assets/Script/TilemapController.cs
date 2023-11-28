7a29c98dc03e1dfd792e0e0a2bf3ca0e0166b854
Rainvice <3218229723@qq.com>
2023-11-28 21:04:36 +0800

    添加放置物品脚本

--- Assets/Script/TilemapController.cs
+++ Assets/Script/TilemapController.cs
@@ -4,11 +4,17 @@ using UnityEngine.Tilemaps;
 
 public class TilemapController : MonoBehaviour
 {
+    // ****************** 引用 ******************
+    // 绿底瓦片
     public Tile tileGreen;
+    // 正常瓦片
     public Tile tileNormal;
-
+    // 地图
     private Tilemap m_tilemap;
-    private Vector3Int m_preVector3Int = Vector3Int.zero;
+
+    // ****************** 变量 *****************
+    // 上一步滑过的坐标
+    private Vector3Int m_PreVector3Int = Vector3Int.zero;
 
 
     private void Start()
@@ -19,31 +25,45 @@ public class TilemapController : MonoBehaviour
     private void Update()
     {
         OnMouseToTileListener();
-        
-        
-        
     }
 
     private void OnMouseToTileListener()
     {
+        // 如果没有预选中物体，则直接返回
+        if (GameManager.Instance.CurSelectedObject == null) return;
+        // 获取坐标
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int cellPos = m_tilemap.WorldToCell(mousePos);
-        if (m_preVector3Int != cellPos && m_tilemap.HasTile(cellPos))
+        
+        if (m_PreVector3Int != cellPos && m_tilemap.HasTile(cellPos))
         {
             OnMouseEnterTile(cellPos);
-            OnMouseExitTile(m_preVector3Int);
-            m_preVector3Int = cellPos;
+            OnMouseExitTile(m_PreVector3Int);
+            m_PreVector3Int = cellPos;
         }
     }
 
+    /// <summary>
+    /// 鼠标滑入瓦片事件
+    /// </summary>
+    /// <param name="cellPos">瓦片的坐标</param>
     private void OnMouseEnterTile(Vector3Int cellPos)
     {
-        m_tilemap.SetTile(cellPos, tileGreen);
+        // m_tilemap.SetTile(cellPos, tileGreen);
+        if (GameManager.Instance.CurSelectedObject!= null)
+        {
+            // 实例化的预制体的位置
+            GameManager.Instance.CurSelectedObject.transform.position = cellPos;
+        }
     }
 
+    /// <summary>
+    /// 鼠标滑出瓦片的事件
+    /// </summary>
+    /// <param name="cellPos">瓦片的坐标</param>
     private void OnMouseExitTile(Vector3Int cellPos)
     {
-        m_tilemap.SetTile(cellPos, tileNormal);
+        // m_tilemap.SetTile(cellPos, tileNormal);
     }
     
 
---
+++ Assets/Script/UI/MenuButton.cs
@@ -0,0 +1,61 @@
+﻿using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuButton : MonoBehaviour
+{
+    
+    // ************* 引用
+    // 主页
+    public Button btnHome;
+    public GameObject Home;
+    // 城墙
+    public Button BtnWall;
+    public GameObject Wall;
+    // 炮台
+    public Button btnTurret;
+    public GameObject turret;
+    // 铁矿采集器
+    public Button btnMining;
+    public GameObject mining;
+    // 金矿采集器
+    public Button btnCion;
+    public GameObject cion;
+    // 炮弹工厂
+    public Button btnBullet;
+    public GameObject bullet;
+    // 运输线路
+    public Button btnWay;
+    public GameObject way;
+    // 砍树机器（斧头）
+    public Button btnAxe;
+    public GameObject axe;
+    // 木材加工厂
+    public Button btnHarvester;
+    public GameObject harvester;
+    // 铁矿加工厂
+    public Button btnFactory;
+    public GameObject factory;
+
+    public void Start()
+    {
+        OnClickListener();
+    }
+
+
+
+    private void OnClickListener()
+    {
+        btnHome.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = Home );
+        BtnWall.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = Wall);
+        btnTurret.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = turret);
+        btnMining.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = mining);
+        btnCion.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = cion);
+        btnBullet.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = bullet);
+        btnWay.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = way);
+        btnAxe.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = axe);
+        btnHarvester.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = harvester);
+        btnFactory.onClick.AddListener(() => GameManager.Instance.CurSelectedObject = factory);
+    }
+
+}
