c08fd53a394b33cf19f711cc7151a68edf0bf0d6
Rainvice <3218229723@qq.com>
2023-11-28 22:09:09 +0800

    修复坐标偏移问题

---
+++ Assets/Script/Data/Base/BaseBuild.cs
@@ -0,0 +1,31 @@
+﻿
+public abstract class BaseBuild
+{
+    // ***************** 变量 ******************
+    // 范围
+    private int distance;
+    // 等级
+    private int level;
+    // 是否放置
+    private bool isPlace = false;
+    // 类型
+    private BuildType buildType;
+    // 生命
+    private int hp;
+    // 升级价格
+    private int upgradePrice;
+    // 价格
+    private int price;
+    
+}
+
+
+public enum BuildType
+{
+    // 攻击
+    Attack,
+    // 防御
+    Defense,
+    // 生产
+    Production
+}
--- Assets/Script/TilemapController.cs
+++ Assets/Script/TilemapController.cs
@@ -11,15 +11,18 @@ public class TilemapController : MonoBehaviour
     public Tile tileNormal;
     // 地图
     private Tilemap m_tilemap;
+    // 网格
+    private Grid m_Grid;
 
     // ****************** 变量 *****************
     // 上一步滑过的坐标
     private Vector3Int m_PreVector3Int = Vector3Int.zero;
 
 
-    private void Start()
+    private void Awake()
     {
-        m_tilemap = GetComponent<Tilemap>();
+        m_tilemap = GetComponentInChildren<Tilemap>();
+        m_Grid = GetComponent<Grid>();
     }
 
     private void Update()
@@ -27,6 +30,9 @@ public class TilemapController : MonoBehaviour
         OnMouseToTileListener();
     }
 
+    /// <summary>
+    /// 鼠标在瓦片上事件
+    /// </summary>
     private void OnMouseToTileListener()
     {
         // 如果没有预选中物体，则直接返回
@@ -34,7 +40,7 @@ public class TilemapController : MonoBehaviour
         // 获取坐标
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int cellPos = m_tilemap.WorldToCell(mousePos);
-        
+        // 如果坐标改变，则触发事件
         if (m_PreVector3Int != cellPos && m_tilemap.HasTile(cellPos))
         {
             OnMouseEnterTile(cellPos);
@@ -49,23 +55,14 @@ public class TilemapController : MonoBehaviour
     /// <param name="cellPos">瓦片的坐标</param>
     private void OnMouseEnterTile(Vector3Int cellPos)
     {
-        // m_tilemap.SetTile(cellPos, tileGreen);
-        if (GameManager.Instance.CurSelectedObject!= null)
-        {
-            // 实例化的预制体的位置
-            GameManager.Instance.CurSelectedObject.transform.position = cellPos;
-        }
+        // 实例化的预制体的位置
+        if (GameManager.Instance.CurSelectedObject != null)
+            GameManager.Instance.CurSelectedObject.transform.position = m_Grid.CellToWorld(cellPos);
     }
 
     /// <summary>
     /// 鼠标滑出瓦片的事件
     /// </summary>
     /// <param name="cellPos">瓦片的坐标</param>
-    private void OnMouseExitTile(Vector3Int cellPos)
-    {
-        // m_tilemap.SetTile(cellPos, tileNormal);
-    }
-    
-
-    
+    private void OnMouseExitTile(Vector3Int cellPos) { }
 }
