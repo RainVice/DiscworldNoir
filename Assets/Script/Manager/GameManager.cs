ebec906f2359e66c4302628e94f7467679e2d4b3
Rainvice <3218229723@qq.com>
2023-11-29 18:27:33 +0800

    增加随机生成地形

--- Assets/Script/Manager/GameManager.cs
+++ Assets/Script/Manager/GameManager.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,95 +7,128 @@ using UnityEngine;
 /// </summary>
 public class GameManager : MonoBehaviour
 {
-
-    public static GameManager Instance => m_instance;
+    public static GameManager Instance => m_Instance;
 
     // ************** 属性 *****************
     public GameObject CurSelectedObject
     {
-        get => m_curSelectedObject;
+        get => m_CurSelectedObject;
         // 实例化预制体
-        set => m_curSelectedObject = value;
+        set => m_CurSelectedObject = value;
     }
-    
     // *************** 变量 *****************
     // 单例
-    private static GameManager m_instance;
+    private static GameManager m_Instance;
+
     // 当前选择的一个物体
-    private GameObject m_curSelectedObject;
+    private GameObject m_CurSelectedObject;
+
     // 建筑集合分类
-    private Dictionary<Type,List<BaseBuild>> m_builds;
+    private Dictionary<Type, List<BaseBuild>> m_Builds;
+
     // 建筑集合
-    private Dictionary<Vector3Int,BaseBuild> m_buildList;
+    private Dictionary<Vector3Int, BaseBuild> m_BuildList;
+
+    // 地形集合
+    private Dictionary<Vector3Int, BaseTerrain> m_TerrainList;
 
     private void Awake()
     {
-        m_instance = this;
+        m_Instance = this;
     }
     
+    // 是否存在地形
+    public bool HasTerrain(Vector3Int cellPos)
+    {
+        if (m_TerrainList == null)
+        {
+            m_TerrainList = new Dictionary<Vector3Int, BaseTerrain>();
+        }
+        return m_TerrainList.ContainsKey(cellPos);
+    }
+    
+    /// <summary>
+    /// 添加地形
+    /// </summary>
+    /// <param name="cellPos"> tile 坐标</param>
+    /// <param name="terrain"> 地形 </param>
+    public void AddTerrain(Vector3Int cellPos, GameObject terrain)
+    {
+        if (m_TerrainList == null)
+        {
+            m_TerrainList = new Dictionary<Vector3Int, BaseTerrain>();
+        }
+        var baseTerrain = terrain.GetComponent<BaseTerrain>();
+        m_TerrainList.Add(cellPos, baseTerrain);
+    }
+
     /// <summary>
     /// 添加建筑
     /// </summary>
-    /// <param name="cellPos"></param>
-    /// <param name="build"></param>
-    public void AddBuild(Vector3Int cellPos,GameObject build)
+    /// <param name="cellPos">tile 坐标</param>
+    /// <param name="build">建筑物</param>
+    public void AddBuild(Vector3Int cellPos, GameObject build)
     {
-        if (m_buildList == null)
+        if (m_BuildList == null)
         {
-            m_buildList = new Dictionary<Vector3Int, BaseBuild>();
+            m_BuildList = new Dictionary<Vector3Int, BaseBuild>();
         }
+
         BaseBuild baseBuild = build.GetComponent<BaseBuild>();
-        m_buildList.Add(cellPos,baseBuild);
+        m_BuildList.Add(cellPos, baseBuild);
     }
-    
+
     /// <summary>
     /// 返回指定坐标的建筑
     /// </summary>
-    /// <param name="cellPos"></param>
-    /// <returns></returns>
+    /// <param name="cellPos">tile 坐标</param>
+    /// <returns>建筑物</returns>
     public BaseBuild GetBuild(Vector3Int cellPos)
     {
-        if (m_buildList == null)
+        if (m_BuildList == null)
         {
             return null;
         }
-        return m_buildList.TryGetValue(cellPos, out var build) ? build : null;
+
+        return m_BuildList.TryGetValue(cellPos, out var build) ? build : null;
     }
-    
-    
+
+
     /// <summary>
     /// 添加游戏对象进入字典中保存，方便下次批量调用
     /// </summary>
-    /// <param name="build"></param>
+    /// <param name="build">建筑物</param>
     public void AddBuild(GameObject build)
     {
-        if (m_builds == null)
+        if (m_Builds == null)
         {
-            m_builds = new Dictionary<Type, List<BaseBuild>>();
+            m_Builds = new Dictionary<Type, List<BaseBuild>>();
         }
+
         BaseBuild baseBuild = build.GetComponent<BaseBuild>();
-        if (!m_builds.ContainsKey(baseBuild.GetType()))
+        if (!m_Builds.ContainsKey(baseBuild.GetType()))
         {
-            m_builds.Add(baseBuild.GetType(), new List<BaseBuild>());
+            m_Builds.Add(baseBuild.GetType(), new List<BaseBuild>());
         }
-        m_builds[baseBuild.GetType()].Add(baseBuild);
+
+        m_Builds[baseBuild.GetType()].Add(baseBuild);
     }
-    
+
     /// <summary>
     /// 在字典中删除指定游戏对象
     /// </summary>
-    /// <param name="build"></param>
+    /// <param name="build">建筑物</param>
     public void RemoveBuild(GameObject build)
     {
-        if (m_builds == null)
+        if (m_Builds == null)
         {
             return;
         }
+
         BaseBuild baseBuild = build.GetComponent<BaseBuild>();
-        if (m_builds.ContainsKey(baseBuild.GetType()))
+        if (m_Builds.ContainsKey(baseBuild.GetType()))
         {
-            m_builds[baseBuild.GetType()].Remove(baseBuild);
+            m_Builds[baseBuild.GetType()].Remove(baseBuild);
         }
     }
-    
 }
--- Assets/Script/TilemapController.cs
+++ Assets/Script/TilemapController.cs
@@ -1,5 +1,3 @@
-using System;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
@@ -18,8 +16,12 @@ public class TilemapController : MonoBehaviour
     private Tilemap m_Tilemap;
     // 网格
     private Grid m_Grid;
+    // 地形
+    public GameObject[] m_Terrains;
 
     // ****************** 变量 *****************
+    // 鼠标在上一帧的位置
+    private Vector3 m_PreMousePos = Vector3.zero;
     // 上一步滑过的坐标
     private Vector3Int m_PreVector3Int = Vector3Int.zero;
     // 选择的预制体
@@ -36,14 +38,45 @@ public class TilemapController : MonoBehaviour
         m_Grid = GetComponent<Grid>();
     }
 
+    private void Start()
+    {
+        // 生成地形
+        for (int i = 0; i < 500; i++)
+        {
+            var randomPoint = GenerateRandomPoint();
+            var instantiate = Instantiate(
+                m_Terrains[i % m_Terrains.Length], 
+                m_Grid.CellToWorld(randomPoint),
+                Quaternion.identity);
+            GameManager.Instance.AddTerrain(randomPoint,instantiate);
+        }
+    }
+
     private void Update()
     {
         OnMouseToTileListener();
         OnMouseDownListener();
     }
 
-    public void OnMouseDownListener()
+    /// <summary>
+    /// 鼠标按住监听
+    /// </summary>
+    private void OnMouseListener()
     {
+        if (Input.GetMouseButton(0))
+        {
+            
+        }
+    }
+    
+    
+    
+    /// <summary>
+    /// 鼠标按下监听
+    /// </summary>
+    private void OnMouseDownListener()
+    {
+        m_PreMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         if (Input.GetMouseButtonDown(0))
         {
             // 鼠标在UI上直接返回
@@ -78,7 +111,7 @@ public class TilemapController : MonoBehaviour
         // 获取坐标
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var cellPos = m_Tilemap.WorldToCell(mousePos);
-        // 如果选择的预制体与GameManager的不同，弃用当前的
+        // 如果选择的预制体与 GameManager 的不同，弃用当前的
         if (m_CurSelectedObject!= GameManager.Instance.CurSelectedObject)
         {
             m_CurSelectedObject = GameManager.Instance.CurSelectedObject;
@@ -123,4 +156,18 @@ public class TilemapController : MonoBehaviour
     /// </summary>
     /// <param name="cellPos">瓦片的坐标</param>
     private void OnMouseExitTile(Vector3Int cellPos) { }
+    
+    /// <summary>
+    /// 生成随机坐标
+    /// </summary>
+    /// <returns></returns>
+    private Vector3Int GenerateRandomPoint()
+    {
+        Vector3Int randomPoint;
+        do
+        {
+            randomPoint = new Vector3Int( Random.Range(-70,70), Random.Range(-70,70), 0);
+        } while (GameManager.Instance.HasTerrain(randomPoint));
+        return randomPoint;
+    }
 }
